5f647f095aaf0bfe6fbda639921dc24f5d9c679c
tatarsupakit <proinwmak55@gmail.com>
2023-03-31 09:21:52 +0700

    added_jsdoc

--- laravelBackend/app/Http/Controllers/AuthController.php
+++ laravelBackend/app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -8,15 +9,37 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     *
+     *Returns the profile information of the currently authenticated user.
+     *@function
+     *@returns {Object} - The profile information of the authenticated user.
+     *@throws {Error} - Throws an error if the user is not authenticated.
+     */
     public function Profile()
     {
         return auth()->user();
     }
-    public function register(Request $request){
+
+    /**
+     *
+     *Registers a new user with the given information.
+     *@function
+     *@async
+     *@param {Object} request - The HTTP request object containing the user information.
+     *@param {string} request.name - The name of the user.
+     *@param {string} request.email - The email of the user.
+     *@param {string} request.passwosrd - The password of the user.
+     *@param {string} request.password_confirmation - The confirmation of the user's password.
+     *@return {Promise<Object>} - The HTTP response containing the newly created user and token.
+     *@throws {Error} - Throws an error if any of the required fields are missing or the email is already taken.
+     */
+    public function register(Request $request)
+    {
         $fields = $request->validate([
             'name' => 'required|string',
-            'email' =>'required|string|unique:users,email',
-            'password'=>'required|string|confirmed'
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
         ]);
         $user = User::create([
             'name' => $fields['name'],
@@ -25,56 +48,58 @@ class AuthController extends Controller
         ]);
         $token = $user->createToken('myapptoken')->plainTextToken;
 
-       
-        $response=[
-            'user'=>$user,
-            'token'=>$token
+
+        $response = [
+            'user' => $user,
+            'token' => $token
         ];
-        return response($response,201);
+        return response($response, 201);
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $fields = $request->validate([
-            
-            'email' =>'required|string',
-            'password'=>'required|string'
+
+            'email' => 'required|string',
+            'password' => 'required|string'
         ]);
 
-        
-        $user = User::where('email',$fields['email'])->first();
 
-        if(!$user||!Hash::check($fields['password'],$user->password)){
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
             return response([
-                'message'=>'Bad creds'
-            ],401);
+                'message' => 'Bad creds'
+            ], 401);
         }
-            
-            $user->tokens()->delete();
-            $token = $user->createToken('myapptoken')->plainTextToken;
-            $response=[
-                    'user'=>$user,
-                    'token'=>$token,
-                    'status'=>'ok'
-                ];
-                return response($response,201);
-        }
-        
 
-        //$token = $user->createToken('myapptoken')->plainTextToken;
-        
-        //dd($user->tokens()->count());
-        // $response=[
-        //     'user'=>$user,
-        //     'token'=>$token,
-        //     'status'=>'ok'
-        // ];
-        // return response($response,201);
-    
+        $user->tokens()->delete();
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token,
+            'status' => 'ok'
+        ];
+        return response($response, 201);
+    }
 
-    public function logout(Request $request){
+
+    //$token = $user->createToken('myapptoken')->plainTextToken;
+
+    //dd($user->tokens()->count());
+    // $response=[
+    //     'user'=>$user,
+    //     'token'=>$token,
+    //     'status'=>'ok'
+    // ];
+    // return response($response,201);
+
+
+    public function logout(Request $request)
+    {
         auth()->user()->tokens()->delete();
-        return[
-            'message'=>'Logged out'
+        return [
+            'message' => 'Logged out'
         ];
     }
 }
--- laravelBackend/app/Models/User.php
+++ laravelBackend/app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'password_confirmation',
     ];
 
     /**
@@ -42,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 
+     */
+    protected $table = 'users';
 }
