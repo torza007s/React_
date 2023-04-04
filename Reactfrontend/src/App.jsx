65d82e4b7195ef3787d4652ad627bf19d2272024
torza007 <tor98795@gmail.com>
2023-04-04 15:31:48 +0700

    fix css

--- Reactfrontend/src/App.jsx
+++ Reactfrontend/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+    <table class="border-separate border-spacing-2 border border-slate-400 ...">
+  <thead>
+    <tr>
+      <th class="border border-slate-300 ..."><p className="read-the-docs">State</p></th>
+      <th class="border border-slate-300 ...">City</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-slate-300 ...">Indiana</td>
+      <td class="border border-slate-300 ...">Indianapolis</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Ohio</td>
+      <td class="border border-slate-300 ...">Columbus</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Michigan</td>
+      <td class="border border-slate-300 ...">Detroit</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   )
 }
