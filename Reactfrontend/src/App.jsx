7be2ebe9bc47cf07613085e71cc9fffa501de070
torza007 <tor98795@gmail.com>
2023-04-10 16:57:49 +0700

    10/04/23

--- Reactfrontend/src/App.jsx
+++ Reactfrontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Header from './componect/dashboard/Header'
-import Sidebar from './componect/dashboard/Sidebar'
+import Navbar from './componect/Navbar'
+
+
 export default function App() {
   return (
     <div>
-      <Sidebar>
-      <div className='bg-gray-600 min-h-screen' >
-      <Header />
-      </div>
-      </Sidebar>
+      
+    <Navbar />
 
     </div>
   )
